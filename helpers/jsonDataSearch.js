defe7c3754418f8f096463e2fbced9d10a676a71
Caleb Trachte <cetrachte@hotmail.com>
2020-12-15 15:31:07 -0600

    add code to get json data from folder, doesnt work :(

--- helpers/jsonDataSearch.js
+++ helpers/jsonDataSearch.js
@@ -1,30 +1,90 @@
-$('#txt-search').keyup(function(){
-            var searchField = $(this).val();
-			if(searchField === '')  {
-				$('#filter-records').html('');
-				return;
-			}
-			
-            var regex = new RegExp(searchField, "i");
-            var output = '<div class="row">';
-            var count = 1;
-			  $.each(data, function(key, val){
-				if ((val.employee_salary.search(regex) != -1) || (val.employee_name.search(regex) != -1)) {
-				  output += '<div class="col-md-6 well">';
-				  output += '<div class="col-md-3"><img class="img-responsive" src="'+val.profile_image+'" alt="'+ val.employee_name +'" /></div>';
-				  output += '<div class="col-md-7">';
-				  output += '<h5>' + val.employee_name + '</h5>';
-				  output += '<p>' + val.employee_salary + '</p>'
-				  output += '</div>';
-				  output += '</div>';
-				  if(count%2 == 0){
-					output += '</div><div class="row">'
-				  }
-				  count++;
-				}
-			  });
-			  output += '</div>';
-			  $('#filter-records').html(output);
-        });
+// var data = [];
+// let airportData;
 
-        //https://www.js-tutorials.com/jquery-tutorials/live-search-json-objects-data-using-jquery/
+// function loadJSON(callback) {
+
+//     var httpReq = new XMLHttpRequest();
+//     httpReq.overrideMimeType("application/json");
+//     httpReq.open('GET', './assets/data/airports.json', true);
+//     httpReq.setRequestHeader('Access-Control-Allow-Headers', '*');
+//     httpReq.setRequestHeader('Content-type', 'application/ecmascript');
+//     httpReq.setRequestHeader('Access-Control-Allow-Origin', '*');
+//     httpReq.onreadystatechange = function () {
+//         if (httpReq.readyState == 4 && httpReq.status == "200") {
+//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+//             callback(httpReq.responseText);
+//         }
+//     };
+//     httpReq.send(null);
+// }
+
+// function init() {
+//     loadJSON(function (response) {
+//         // Parsing JSON string into object
+//         airportData = JSON.parse(response);
+//         for (var i in airportData) {
+//             data.push([i, airportData[i]]);
+//         }
+//     });
+// }
+// init();
+
+// This code above doesnt work!
+
+$('#txt-search-destination').keyup(function () {
+    var searchField = $(this).val();
+    if (searchField === '') {
+        $('#filter-records').html('');
+        return;
+    }
+
+    var regex = new RegExp(searchField, "i");
+    var output = '<div class="row">';
+    var count = 1;
+    $.each(data, function (key, val) {
+        if ((val.city.search(regex) != -1) || (val.state.search(regex) != -1)) {
+            output += '<div class="col-md-6 well">';
+            output += '<div class="col-md-7">';
+            output += '<h5>' + val.city + '</h5>';
+            output += '<p>' + val.state + '</p>'
+            output += '</div>';
+            output += '</div>';
+            if (count % 2 == 0) {
+                output += '</div><div class="row">'
+            }
+            count++;
+        }
+    });
+    output += '</div>';
+    $('#filter-records').html(output);
+});
+
+$('#txt-search-origin').keyup(function () {
+    var searchField = $(this).val();
+    if (searchField === '') {
+        $('#filter-records').html('');
+        return;
+    }
+
+    var regex = new RegExp(searchField, "i");
+    var output = '<div class="row">';
+    var count = 1;
+    $.each(data, function (key, val) {
+        if ((val.city.search(regex) != -1) || (val.state.search(regex) != -1)) {
+            output += '<div class="col-md-6 well">';
+            output += '<div class="col-md-7">';
+            output += '<h5>' + val.city + '</h5>';
+            output += '<p>' + val.state + '</p>'
+            output += '</div>';
+            output += '</div>';
+            if (count % 2 == 0) {
+                output += '</div><div class="row">'
+            }
+            count++;
+        }
+    });
+    output += '</div>';
+    $('#filter-records').html(output);
+});
+
+        //Find more infor and source code @ https://www.js-tutorials.com/jquery-tutorials/live-search-json-objects-data-using-jquery/
